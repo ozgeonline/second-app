219c92fa0da3464bbb86d03adc846c76ecf3609a
ozgeonline <nisroc.414@hotmail.com>
2023-08-05 14:07:14 +0300

    test

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer(){
+  const currentDate = new Date().getFullYear();
+  return <footer>
+    <p>Copyright ⓒ {currentDate}</p>
+  </footer>
+}
+export default Footer;
